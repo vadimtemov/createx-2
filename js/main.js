25c28891b05ce4608c00d355084d2548435a64e7
vadimtemov <128626319+vadimtemov@users.noreply.github.com>
2023-07-12 14:35:03 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 Fancybox.bind("[data-fancybox]", {
     // Your custom options
     });
+
+
+$(function() {
+
+    var mixer = mixitup('.directions__list');
+
+// $('.directions__filter-bth').on('click', function () {
+//     $('.directions__filter-bth').removeClass('directions__filter-bth--active')
+//     $(this).addClass('directions__filter-bth--active')
+// })
+
+// })
+
+$('.directions__filter-bth').on('click', function () {
+    $('.directions__filter-bth').removeClass('directions__filter-bth--active')
+    $(this).addClass('directions__filter-bth--active')
+})
+
+})
