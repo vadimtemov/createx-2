408604ef68a27b0b67485bac11ab5eb668762ff3
vadimtemov <128626319+vadimtemov@users.noreply.github.com>
2023-07-13 11:14:48 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,13 +7,6 @@ $(function() {
 
     var mixer = mixitup('.directions__list');
 
-// $('.directions__filter-bth').on('click', function () {
-//     $('.directions__filter-bth').removeClass('directions__filter-bth--active')
-//     $(this).addClass('directions__filter-bth--active')
-// })
-
-// })
-
 $('.directions__filter-bth').on('click', function () {
     $('.directions__filter-bth').removeClass('directions__filter-bth--active')
     $(this).addClass('directions__filter-bth--active')
