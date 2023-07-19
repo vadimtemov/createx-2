2474385971d2a5e6caf9d4c88d5f6eaaa73db16c
vadimtemov <128626319+vadimtemov@users.noreply.github.com>
2023-07-19 18:29:56 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,7 +17,6 @@ $('.team__slider').slick({
     slidesToShow: 4,
     infinite: true,
     draggable: false,
-    dots: true,
 })
 
 $('.team__slider-prew').on('click', function (e) {
@@ -28,5 +27,22 @@ $('.team__slider-next').on('click', function (e) {
     e.preventDefault()
     $('.team__slider').slick('slickNext')
 })
+
+$('.testimonials__slider').slick({
+    arrows: false,
+    dots: true,
+    appendDots: $('.testimonials__dots'),
+    waitForAnimate: false,
+})
+
+$('.testimonials__prev').on('click', function (e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickPrev')
+})
+$('.testimonials__next').on('click', function (e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickNext')
+})
+
 
 })
